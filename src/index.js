fd12c2d5c7ab6d10813ddf9df3f978bb1bc59d6c
userName <>
2022-06-28 16:34:41 +0500

    modify components

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ import './style/style.scss';
 
 
 
-
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 ReactDOM.render(
   <React.StrictMode>
     <App />
